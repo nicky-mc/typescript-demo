b1db27ab31752f75a378883429427b3d53af7aa0
Nicky Mortoza-Cowles <cinimodazotrom@gmail.com>
2024-11-18 12:59:30 +0000

    Update JavaScriptExample.tsx

--- src/app/components/JavaScriptExample.tsx
+++ src/app/components/JavaScriptExample.tsx
@@ -48,7 +48,7 @@ const JavaScriptExample: React.FC = () => {
         <code>
           {startTyping && (
             <Typewriter
-              words={[`// FizzBuzz Exercise\nfor (let i = 1; i <= 100; i++) {\n  if (i % 15 === 0) console.log('FizzBuzz');\n  else if (i % 3 === 0) console.log('Fizz');\n  else if (i % 5 === 0) console.log('Buzz');\n  else console.log(i);\n}`]}
+              words={[`// FizzBuzz Exercise\nfor (let i = 1; i <= 60; i++) {\n  if (i % 15 === 0) console.log('FizzBuzz');\n  else if (i % 3 === 0) console.log('Fizz');\n  else if (i % 5 === 0) console.log('Buzz');\n  else console.log(i);\n}`]}
               loop={false}
               cursor
               cursorStyle="_"
